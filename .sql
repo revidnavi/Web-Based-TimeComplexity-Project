a457cd495b5b1111e90dfe94e30239fd9633cde8
Jacob Celestino <arjhaycelestino54@gmail.com>
2026-05-08 10:10:40 +0800

    add .sql file

---
+++ .sql
@@ -0,0 +1,63 @@
+CREATE DATABASE aco_db;
+USE aco_db;
+
+CREATE TABLE signup_codes (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+
+  email VARCHAR(32),
+  code INT,
+
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+
+  email VARCHAR(32) UNIQUE,
+  hashed_pass VARCHAR(255),
+  user_type VARCHAR(8) NOT NULL DEFAULT 'normal',
+
+  activated BOOLEAN DEFAULT TRUE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE algorithms (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+
+  algo_name VARCHAR(32),
+  category VARCHAR(16),
+
+  time_best VARCHAR(16),
+  time_avg VARCHAR(16),
+  time_worst VARCHAR(16),
+  space_complexity VARCHAR(16),
+
+  active BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE results (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  algo_id INT,
+
+  input_size INT,
+  execution_time FLOAT,
+  space_usage BIGINT,
+
+  archived BOOLEAN DEFAUlT FALSE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (algo_id) REFERENCES algorithms(id)
+);
+
+INSERT INTO algorithms 
+(algo_name, category, time_best, time_avg, time_worst, space_complexity, active)
+VALUES
+('Bubble Sort', 'Sorting', 'O(n)', 'O(n^2)', 'O(n^2)', 'O(1)', 1),
+('Merge Sort', 'Sorting', 'O(n log n)', 'O(n log n)', 'O(n log n)', 'O(n)', 1),
+('Binary Search', 'Searching', 'O(1)', 'O(log n)', 'O(log n)', 'O(1)', 1),
+('Linear Search', 'Searching', 'O(1)', 'O(n)', 'O(n)', 'O(1)', 1),
+('Recursive Fibonacci', 'Computation', 'O(1)', 'O(2^n)', 'O(2^n)', 'O(n)', 1),
+('Dynamic Fibonacci', 'Computation', 'O(n)', 'O(n)', 'O(n)', 'O(n)', 1); 
